6b758fab9c7ca6bb09db6a8e311e599edf126a4a
Khaled <khaledtareq44@gmail.com>
2020-05-04 08:59:55 +0200

    improve the README file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'posts#index', as: 'home'
+  root 'application#goodbye', as: 'home'
 
   get 'about' => 'pages#about', as: 'about'
 
